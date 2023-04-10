50b39755a7b5e33853973271167a70ca858639fe
capellj2 <james.capellan2@mail.dcu.ie>
2023-04-10 23:52:46 +0100

    Fixed constant GET bug from singlebook

--- Lab_11/myreactexample/frontend/src/components/singlebook.jsx
+++ Lab_11/myreactexample/frontend/src/components/singlebook.jsx
@@ -1,40 +1,32 @@
-import { useEffect, useState }  from "react";
+import { useEffect, useState } from "react";
 
-function SingleBook({id}) {
-    const[info, setInfo] = useState([]);
-    
+function SingleBook({ id }) {
+  const [info, setInfo] = useState([]);
 
-    const displayInfo = () =>{
-        return(
-        <div>
-            <p>Id:{info.id}</p>
-            <p>Title: {info.title}</p>
-            <p>Author: {info.author}</p>
-            <p>Genre: {info.genre}</p>
-            <p>Year: {info.year}</p>
-            <p>Inventory: {info.inventory}</p>
-        </div>
-        )
-    };
+  useEffect(() => {
+    let req = "http://127.0.0.1:8000/api/books/" + id + "/";
+    fetch(req)
+      .then((response) => response.json())
+      .then((data) => {
+        setInfo(data);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
 
-    useEffect(()=>{
-        // our code goes here
-        let req = "http://127.0.0.1:8000/api/books/" + id + "/";
-        fetch(req)
-        .then(response=>response.json())
-        .then(data=>{
-            setInfo(data); // get the array of text out and set it as our state
-        })
-        .catch(err=>console.log(err))
-        }
-    )
-
+  const displayInfo = () => {
     return (
-        <p>
-            {displayInfo()}
-        </p>
-    )
+      <div>
+        <p>Id:{info.id}</p>
+        <p>Title: {info.title}</p>
+        <p>Author: {info.author}</p>
+        <p>Genre: {info.genre}</p>
+        <p>Year: {info.year}</p>
+        <p>Inventory: {info.inventory}</p>
+      </div>
+    );
+  };
 
+  return <p>{displayInfo()}</p>;
 }
 
-export default SingleBook
+export default SingleBook;
